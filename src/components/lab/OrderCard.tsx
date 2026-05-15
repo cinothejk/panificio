c2b65cff8320b9693d4c572ded4acd3f508a0718
cinothejk <marco.diblasi@external.stellantis.com>
2026-05-15 09:29:53 +0200

    Lab page improved: 2

--- src/components/lab/OrderCard.tsx
+++ src/components/lab/OrderCard.tsx
@@ -1,31 +1,37 @@
 "use client";
 
-import { useDraggable } from "@dnd-kit/core";
-
-
+import {
+  useDraggable,
+} from "@dnd-kit/core";
 
 type Props = {
-  order: any;
+  order: {
+    id: string;
+  };
+
   children: React.ReactNode;
+
+  className?: string;
 };
 
 export default function OrderCard({
   order,
   children,
+  className = "",
 }: Props) {
+
   const {
     attributes,
     listeners,
     setNodeRef,
     transform,
-    
   } = useDraggable({
     id: order.id,
   });
 
   const style = transform
     ? {
-        transform: `translate3d(${transform.x}px, ${transform.y}px, 0) `,
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
       }
     : undefined;
 
@@ -35,7 +41,15 @@ export default function OrderCard({
       style={style}
       {...listeners}
       {...attributes}
-      
+      className={`
+        touch-none
+        rounded-3xl
+        shadow-sm
+        p-5
+        bg-white
+        transition
+        ${className}
+      `}
     >
       {children}
     </div>
